2f55a7bac3323f293008691ff842c3ce794aa6c9
iewnfod <108739262+iewnfod@users.noreply.github.com>
2022-09-20 20:02:00 +0800

    Add files via upload

--- web_project/templates/js/support.js
+++ web_project/templates/js/support.js
@@ -43,3 +43,47 @@ function changelan() {
     }
     window.location.href = url
 }
+
+function upLoadFile(fileName, binaryString) {
+    let url = getCurrentUrl() + '/upload'
+    var temp = document.createElement('form')
+    temp.action = url
+    temp.method = 'post'
+    temp.style.display = 'none'
+    var file = document.createElement('textarea')
+    file.name = 'file'
+    file.value = binaryString
+    var file_name = document.createElement('textarea')
+    file_name.value = fileName
+    temp.appendChild(file)
+    temp.appendChild(file_name)
+    document.body.appendChild(temp)
+    temp.submit()
+    return temp
+}
+
+function PostHttpRequest(url, values) {
+    var temp = document.createElement('form')
+    temp.action = url
+    temp.method = 'post'
+    temp.style.display = 'none'
+
+    for (var i in values) {
+        var obj = document.createElement('textarea')
+        obj.name = i
+        obj.value = values[i]
+        temp.appendChild(obj)
+    }
+
+    document.body.appendChild(temp)
+    temp.submit()
+
+    return temp
+}
+
+function getIP() {
+    var s = document.createElement('script')
+    s.src = 'https://pv.sohu.com/cityjson?ie=utf-8'
+    document.body.appendChild(s)
+    return returnCitySN["cip"]
+}
